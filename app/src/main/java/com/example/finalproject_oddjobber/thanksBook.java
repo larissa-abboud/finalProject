14902a7679cc07180314ca675bc5cccbf2639b20
larissa-abboud <lariabboud@gmail.com>
2022-04-23 23:16:57 -0700

    added pages

---
+++ app/src/main/java/com/example/finalproject_oddjobber/thanksBook.java
@@ -0,0 +1,14 @@
+package com.example.finalproject_oddjobber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thanksBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanks_book);
+    }
+}
